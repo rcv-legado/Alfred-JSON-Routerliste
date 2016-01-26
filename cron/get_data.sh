f0ddf42af91fb2f23ef668693f950d19517e5502
Freifunk im Ennepe-Ruhr-Kreis <matthias.fehl@ruhr-uni-bochum.de>
2016-01-26 01:38:50 +0100

    null to unbekannt

--- cron/get_data.sh
+++ cron/get_data.sh
@@ -4,7 +4,7 @@ NODEJSON="/var/www/ffmap/data/spr/nodes.json"
 ALFREDWEB="/var/www/ffmap/sprockhoevel/alfred"
 
 ## Tunneldigger VPN
-cat $NODEJSON | jq '.nodes | .[] | {name: .nodeinfo.hostname, kontakt: .nodeinfo.owner.contact, online: .flags.online, vpn: .nodeinfo.software.tunneldigger.enabled, adresse: .nodeinfo.network.addresses}' | grep -v fe80 | sed '/2a03/s/,//g' | sed '$d;1d' | sed 's/}/},/' | sed '1 i\[{' | sed '$ c\]}]' >$ALFREDWEB/knotenliste.json
+cat $NODEJSON | jq '.nodes | .[] | {name: .nodeinfo.hostname, kontakt: .nodeinfo.owner.contact, online: .flags.online, vpn: .nodeinfo.software.tunneldigger.enabled, adresse: .nodeinfo.network.addresses}' | grep -v fe80 | sed '/2a03/s/,//g' | sed '$d;1d' | sed 's/}/},/' | sed 's/null/"unbekannt"/' | sed '1 i\[{' | sed '$ c\]}]' >$ALFREDWEB/knotenliste.json
 
 ## fastd -- ungetestet!
-#cat $NODEJSON | jq '.nodes | .[] | {name: .nodeinfo.hostname, kontakt: .nodeinfo.owner.contact, online: .flags.online, vpn: .nodeinfo.software.fastd.enabled, adresse: .nodeinfo.network.addresses}' | grep -v fe80 | sed '/2a03/s/,//g' | sed '$d;1d' | sed 's/}/},/' | sed '1 i\[{' | sed '$ c\]}]' >$ALFREDWEB/knotenliste.json
+#cat $NODEJSON | jq '.nodes | .[] | {name: .nodeinfo.hostname, kontakt: .nodeinfo.owner.contact, online: .flags.online, vpn: .nodeinfo.software.fastd.enabled, adresse: .nodeinfo.network.addresses}' | grep -v fe80 | sed '/2a03/s/,//g' | sed '$d;1d' | sed 's/}/},/' | sed 's/null/"unbekannt"/' | sed '1 i\[{' | sed '$ c\]}]' >$ALFREDWEB/knotenliste.json
